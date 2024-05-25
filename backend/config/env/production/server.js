b2ed02b164b4930593ebfdbdc427c8967ff66bcc
Timur Galiakabarov <t.gal@pnlfin.tech>
2024-05-25 16:29:44 +0300

    sql connection for strapi

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: ["key1", "key2"]
   },
   url: env('STRAPI_URL'),
 });
